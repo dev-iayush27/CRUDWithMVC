e9cd6b78eda2813196dcb0ce28d41e3e484f7b5a
Ayush Gupta <dev.iayush27@gmail.com>
2024-06-02 17:06:45 +0530

    db conection changes

--- crud/dbConnection.js
+++ crud/dbConnection.js
@@ -4,8 +4,10 @@ const { mongoose } = require("mongoose");
 async function connectMongoDb() {
   return mongoose
     .connect("mongodb://127.0.0.1:27017/my-app")
-    .then(
-      (con) => `MongoDB Database connected with HOST: ${con.connection.host}`
+    .then((con) =>
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      )
     )
     .catch((err) => console.log("Error: ", err));
 }
